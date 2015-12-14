3e3cc1a1f86530ce032337013a4e817c8513853b
xaviuzz <xaviuzz@gmail.com>
2015-12-14 20:15:42 +0100

    Rules extracted ugly

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,6 +1,7 @@
 require './neighbourhood'
 require './state'
 require './scenario'
+require './rule'
 
 class GameOfLife
 
@@ -26,46 +27,5 @@ class GameOfLife
 
     
     return scenario
-  end
-end
-
-class Rule
-  def self.apply scenario
-    return scenario
-  end
-end
-
-
-class UnderPopulation < Rule
-  def self.apply scenario
-    if scenario.state.alive? and scenario.neighbourhood.underpopulated?
-      scenario.resolve(State.dead)
-    end
-  end
-end
-
-
-class OverCrowded < Rule
-  def self.apply scenario
-    if scenario.state.alive? and scenario.neighbourhood.overcrowded?
-      scenario.resolve(State.dead)
-    end
-  end
-end
-
-class Healthy < Rule
-  def self.apply scenario
-    if scenario.state.alive? and scenario.neighbourhood.healthy?
-      scenario.resolve(State.alive)
-    end
-  end
-end
-
-
-class Flourish < Rule
-  def self.apply scenario
-    if scenario.state.dead? and scenario.neighbourhood.flourishing?
-      scenario.resolve(State.alive)
-    end
   end
 end

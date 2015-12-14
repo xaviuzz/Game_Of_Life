3203949d6a05bb40d00f7479d9c920d1a33cb18f
xaviuzz <xaviuzz@gmail.com>
2015-12-15 00:39:30 +0100

    reset

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -3,28 +3,28 @@ require 'rspec'
 
 describe "In the Conway's game of life " do
 
-	it "Any live cell with fewer than two live neighbours dies, as if caused by underpopulation" do
+  xit "Any live cell with fewer than two live neighbours dies, as if caused by underpopulation" do
     neighbours = 1
     expect(fewerThanTwo?(neighbours)).to eq true
     expect(GameOfLife.evaluate(:live_cell,neighbours)).to eq :dead_cell
     expect(GameOfLife.evaluate(:dead_cell,neighbours)).to eq :dead_cell
-	end
+  end
 
-  it "Any live cell with more than three live neighbours dies, as if by overcrowding" do
+  xit "Any live cell with more than three live neighbours dies, as if by overcrowding" do
     neighbours = 4
     expect(moreThanThree?(neighbours)).to eq true
     expect(GameOfLife.evaluate(:live_cell,neighbours)).to eq :dead_cell
     expect(GameOfLife.evaluate(:dead_cell,neighbours)).to eq :dead_cell
   end
 
-  it "Any live cell with two or three live neighbours lives on to the next generation" do
+  xit "Any live cell with two or three live neighbours lives on to the next generation" do
     neighbours = 2
     withTwoOrThree! neighbours
     expect(GameOfLife.evaluate(:live_cell,neighbours)).to eq :live_cell
     expect(GameOfLife.evaluate(:dead_cell,neighbours)).to eq :dead_cell
   end
 
-  it "Any dead cell with exactly three live neighbours becomes a live cell." do
+  xit "Any dead cell with exactly three live neighbours becomes a live cell." do
     three_live_neighbours = 3
     expect(GameOfLife.evaluate(:live_cell,three_live_neighbours)).to eq :live_cell
     expect(GameOfLife.evaluate(:dead_cell,three_live_neighbours)).to eq :live_cell

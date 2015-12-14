2a815900d7db4c04d44e082c3090b64d54591ddb
xaviuzz <xaviuzz@gmail.com>
2015-12-14 19:27:35 +0100

    Bussiness is done, refac expected

--- game_of_life.rb
+++ game_of_life.rb
@@ -6,9 +6,15 @@ class GameOfLife
   def self.evaluate (current_state, live_neighbours)
     state = State.create(current_state)
   	neighbourhood = Neighbourhood.new(live_neighbours)
-    result = State.dead
-  	result = State.alive if state.alive? and neighbourhood.healthy?
-    result = State.alive if state.dead? and neighbourhood.flourishing?
+
+    result = apply_rules state,neighbourhood
     result.to_s
+  end
+
+  def apply_rules state, neighbourhood
+    result = State.dead
+    result = State.alive if state.alive? and neighbourhood.healthy?
+    result = State.alive if state.dead? and neighbourhood.flourishing?
+    result
   end
 end

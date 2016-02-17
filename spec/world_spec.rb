74755835781072517bca2d676ecadeaeaf2099d9
xaviuzz <xaviuzz@gmail.com>
2016-02-17 22:46:57 +0100

    World provides scenarios

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -13,5 +13,42 @@ describe "The world of the Conway's game of life" do
       expect(World.new(empty_world).population).to eq 0
   end
 
+  it "Alive cells are X" do
+    solitary_cell=
+      """...
+          ..X
+          ...
+      """
+      expect(World.new(solitary_cell).population).to eq 1
+  end
+
+  it "Provides scenarios" do
+    solitary_cell=
+      """...
+          ..X
+          ...
+      """
+      world= World.new solitary_cell
+      expect(world.next).to eq [:dead_cell,0]
+      expect(world.next).to eq [:dead_cell,1]
+      expect(world.next).to eq [:dead_cell,1]
+      expect(world.next).to eq [:dead_cell,0]
+      expect(world.next).to eq [:dead_cell,1]
+      expect(world.next).to eq [:live_cell,0]
+      expect(world.next).to eq [:dead_cell,0]
+      expect(world.next).to eq [:dead_cell,1]
+      expect(world.next).to eq [:dead_cell,1]
+  end
+
+  it "Notifies the end of the world" do
+    solitary_cell=
+      """...
+          ..X
+          ...
+      """
+      world= World.new solitary_cell
+      9.times{world.next}
+      expect{world.next}.to raise_error "It's the end of the world"
+  end
 
 end
--- world.rb
+++ world.rb
@@ -1,7 +1,39 @@
 class World
+  
   def initialize descriptor
+    @descriptor = descriptor
+    @index=0
   end
+
   def population
-    0
+    @descriptor.count "X"
+  end
+
+  def next
+    raise "It's the end of the world" if @index==9
+    state=obtain_state @index
+    population = alive_around(@index)
+    @index+=1
+    return[state, population]
+  end
+
+  private
+  
+  def obtain_state index
+    return :live_cell if index==5
+    :dead_cell
+  end
+  
+  def alive_around index
+    region = obtain_region (index)
+    region.count"X"
+  end
+
+  def obtain_region index
+    populated_region="X"
+    empty_region=""
+    with_neighbours=[1,2,4,7,8]
+    return populated_region if  with_neighbours.include? index 
+    empty_region
   end
 end
